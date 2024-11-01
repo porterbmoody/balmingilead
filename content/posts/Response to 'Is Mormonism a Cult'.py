d8fc325a056b49b86d703b0b623df5f5a808c57d
porter <porterbmoody@gmail.com>
2024-11-01 16:47:03 -0400

    changes

---
+++ content/posts/Response to 'Is Mormonism a Cult'.py
@@ -0,0 +1,8 @@
+
+
+the Bible says there’s only one God in all existence (Isaiah 43:10; 44:6; 45:5), but Mormonism says there are many gods, (Mormon Doctrine, p. 163)
+
+# Isaiah 43:10
+
+> Ye are my witnesses, saith the LORD, and my servant whom I have chosen: that ye may know and believe me, and understand that I am he: before me there was no God formed, neither shall there be after me
+
