f94116dbe6f89b1541d86de7a5bdbdc85863a81b
porter <porterbmoody@gmail.com>
2024-11-01 16:37:25 -0400

    changes

---
+++ content/posts/Large Language Models.py
@@ -0,0 +1,5 @@
+
+
+# Neural Network
+A neural network is a machine learning algorithm that uses a network of interconnected nodes, called neurons, to process data in a way that mimics the human brain.
+
