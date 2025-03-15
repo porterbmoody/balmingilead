978a82c9a11fb7484e6a66b8033f9c34466dc6f3
porter <porterbmoody@gmail.com>
2025-03-15 16:48:48 -0700

    f

--- content/posts/book of mormon.py
+++ content/posts/book of mormon.py
@@ -23,15 +23,15 @@ verses = []
 chapters = []
 
 for i, book in enumerate(data['books'][0:2]):
-    book_titles.append(book['book'])
     for chapter in book['chapters']:
         # chapters.append(chapters)
-        chapter_numbers.append(chapter['chapter'])
-        reference_numbers.append(chapter['reference'])
         chapters_verses.append(chapter['verses'])
         for i, chapter_verses in enumerate(chapters_verses):
             # print(chapter_verses)
             for verse in chapter_verses:
+                book_titles.append(book['book'])
+                chapter_numbers.append(chapter['chapter'])
+                reference_numbers.append(chapter['reference'])
                 print(verse['text'])
                 verses.append(verse['text'])
                 verse_references.append(verse['reference'])
@@ -47,9 +47,14 @@ for i, book in enumerate(data['books'][0:2]):
 # print(verses)
 
 
-
 # %%
 
-df_bom = pd.DataFrame({'reference':verse_references,'verse':verses})
+df_bom = (pd.DataFrame({'book_titles':book_titles,'chapter_numbers':chapter_numbers,'reference_numbers':reference_numbers,'reference':verse_references,'verse':verses}))
+
+df_bom['word_count'] = df_bom['verse'].apply(lambda x: len(x.split()))
 df_bom
 
+
+
+
+# %%
