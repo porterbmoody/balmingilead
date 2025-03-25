41cfb33a799928fd577b6313c8d0653c8fe22a6d
porter <porterbmoody@gmail.com>
2025-03-25 15:01:41 -0700

    f

---
+++ content/posts/ml.py
@@ -0,0 +1,27 @@
+
+# #%%
+# pip install -q kokoro>=0.9.2 soundfile
+# # !pip install -q moviepy
+# apt-get -qq -y install espeak-ng > /dev/null 2>&1
+# pip install moviepy==2.0.0.dev2
+# apt install imagemagick
+# cat /etc/ImageMagick-6/policy.xml | sed 's/none/read,write/g'> /etc/ImageMagick-6/policy.xml
+
+
+
+
+# #%%
+
+# from kokoro import KPipeline
+# from IPython.display import display, Audio
+# import soundfile as sf
+# import torch
+# import os
+# import pandas as pd
+# from moviepy.editor import *
+
+
+
+
+
+# %%
