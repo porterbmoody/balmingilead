c33fba761f3540b31a092e5a0bd0850533225ad5
porter <porterbmoody@gmail.com>
2025-03-28 20:24:06 -0700

    f

---
+++ content/posts/create_videos.py
@@ -0,0 +1,31 @@
+pip install -q kokoro>=0.9.2 soundfile
+
+#%%
+# !pip install -q moviepy
+!apt-get -qq -y install espeak-ng > /dev/null 2>&1
+!pip install moviepy==2.0.0.dev2
+!apt install imagemagick
+!cat /etc/ImageMagick-6/policy.xml | sed 's/none/read,write/g'> /etc/ImageMagick-6/policy.xml
+
+#%%
+from kokoro import KPipeline
+from IPython.display import display, Audio
+import soundfile as sf
+import torch
+import os
+import pandas as pd
+from moviepy.editor import *
+from google.colab import drive
+
+drive.mount('/content/drive')
+
+os.makedirs('audio', exist_ok=True)
+os.makedirs('images', exist_ok=True)
+
+
+#%%
+from kokoro import KPipeline
+
+pipeline = KPipeline(lang_code='a')
+pipeline
+
