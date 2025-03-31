128406f2eafa4610bd46c60fb29d4c3152a5b7ea
porter <porterbmoody@gmail.com>
2025-03-31 12:39:07 -0700

    f

--- content/posts/create_videos.py
+++ content/posts/create_videos.py
@@ -35,39 +35,27 @@ from moviepy.editor import *
 import os
 
 url_ids = [
-    'WgibpyTp6dY',
-    'iQ9xvTq3Jk0',
-    'zE_rK8PX83Q',
-    'V90pwWdXC2U',
-    'mqnb0kKsNkQ',
-    'kg8RgTldhbM',
-    'AkzNwAlvq_I',
-    'dGN1bjLajxI',
-    'PQvHFUOc4F4',
-    'q9W76M-I0xI',
-    'Rfhksohebv8',
-    'rYHNB_lPSNc',
-    'Vn-1SX0Prg0',
-    'P48r8D6N14c',
-    'FyASdjZE0R0',
-    'u3VFzuUiTGw',
-    'mZqRDn-8540',
-    'dMq9U44693E',
-    'YdIYBkQBmtA',
-    'ToMj55B6qI8',
-    'qM5b0GeuD0o',
-    'AeCRI306hjo',
-    '1CeuXoX4ndg',
-    'cqFaNi9IMio',
+    # 'EQlv_xUYATg',
+    # 'gSQ-3TD7g_g',
+    # 'OvEKxHqr_b0',
+    # 'qYqtoBRiefk',
+    # 'PWiSXDNtikQ',
+    # 'aSpr8XQNdxo',
+    # 'xQ-xbrgzKB4',
+    # 'cDFBBbhwmfk',
+    # '2NSFmCduZGU',
 ]
 
 for url_id in url_ids:
-
+    print(url_id)
     video = YouTube('https://www.youtube.com/watch?v='+url_id)
     original_title = video.title.replace('?', '').replace('|','').replace('&', 'and').replace('#', '').replace('/','_')
+    print(original_title)
     filename = f'music/{original_title}.wav'
     audio_stream = video.streams.get_audio_only().download()
     audio = AudioFileClip(audio_stream)
     audio.write_audiofile(filename)
     os.remove(audio_stream)
 
+
+# %%
